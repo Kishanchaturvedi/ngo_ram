ab1a5bcd25b11381add0143f48edc8cef9cf41f9
anurag4499 <kisan12chaturvedi@gmail.com>
2023-05-31 12:44:28 +0530

    ngo-project

---
+++ js/payment.js
@@ -0,0 +1,38 @@
+var options = {
+    "key": "rzp_test_kymQ6rJPZnFIzj", // Enter the Key ID generated from the Dashboard
+    "amount": "50000", // Amount is in currency subunits. Default currency is INR. Hence, 50000 refers to 50000 paise
+    "currency": "INR",
+    "name": "WhizzAct Pvt Ltd",
+    "description": "Test Transaction",
+    "image": "http://www.whizzact.com/assets/image/logo.png",
+    // "order_id": "order_IluGWxBm9U8zJ8", //This is a sample Order ID. Pass the `id` obtained in the response of Step 1
+    "handler": function (response) {
+        alert(response.razorpay_payment_id);
+        alert(response.razorpay_order_id);
+        alert(response.razorpay_signature)
+    },
+    "prefill": {
+        "name": "Gaurav Kumar",
+        "email": "gaurav.kumar@example.com",
+        "contact": "9000090000"
+    },
+    "notes": {
+        "address": "Razorpay Corporate Office"
+    },
+    "theme": {
+        "color": "#F1592A"
+    }
+};
+var rzp1 = new Razorpay(options);
+rzp1.on('payment.failed', function (response) {
+    alert(response.error.code);
+    alert(response.error.description);
+    alert(response.error.source);
+    alert(response.error.step);
+    alert(response.error.reason);
+    alert(response.error.metadata.order_id);
+    alert(response.error.metadata.payment_id);
+});
+function razorpay() {
+    rzp1.open();
+}
